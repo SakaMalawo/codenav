07c30e1eb6d49c690826eb68dcc5e1b9b0743f58
SakaMalawo <sakamalawo@gmail.com>
2025-06-06 15:44:53 +0300

    Yee

---
+++ frontend/src/NavbaRR.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1 class="title">{{ message }}</h1>
+  <div class="container">
+    <div><h1>AAAAA</h1></div>
+    <div><h1>BBBB</h1></div>
+    <div><h1>CCCC</h1></div>
+    <div><h1>DDDD</h1></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbaRR",
+  data() {
+    return {
+      message: "Bienvenue sur la page NavbaRR",
+    };
+  },
+};
+</script>
+<style scoped>
+.container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 10px;
+  height: 100vh;
+}
+.title {
+  font-size: 24px;
+  color: #d60f0f;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,4 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import HomeR from "./views/HomeR.vue";
 createApp(HomeR).mount("#HomeR");
+
 createApp(App).mount("#app");
+import NavbaRR from "./NavbaRR.vue";
+createApp(NavbaRR).mount("#NavbaRR");
